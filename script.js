a7103e369d031ff8928d941fcc7e41d640ec6681
Sef van Halbeek <s.vanhalbeek@student.fontys.nl>
2021-12-02 15:06:25 +0100

    V1.0 Styled and finished fetch script

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let mainelement=document.querySelector("main");
+
 // Fetch connected to button that console logs translated response
 document.querySelector("button").addEventListener("click", ()=>{
     console.debug("Doing some magic");
@@ -5,12 +7,25 @@ document.querySelector("button").addEventListener("click", ()=>{
     ((response)=>{
         return response.json();
     }).then((data)=>{
+        // Adds divs with name and website of university to main
+        mainelement.innerHTML="";
         data.forEach(university => {
-            console.log(university.name);
+            console.log(university);
+            
             let div=document.createElement("div");
-            div.innerHTML=university.name;
-            document.querySelector("main").appendChild(div);
-        });
+            let title=document.createElement("div");
+            let content=document.createElement("div");
+
+            div.classList.add("box");
+            title.innerHTML=university.name;
+            title.classList.add("title");
+            content.innerHTML="<a href=\""+university.web_pages[0]+"\" target=\"_blank\">Website</a>";
+            content.classList.add("content");
+
+            div.appendChild(title);    
+            div.appendChild(content); 
+            mainelement.appendChild(div);       
+        })
     })
     );
 });
