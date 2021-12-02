b05540b71aa5fdbf30788b1a3df795518180dde2
Sef van Halbeek <s.vanhalbeek@student.fontys.nl>
2021-12-02 12:15:21 +0100

    V0.2 first fetch test

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// Fetch connected to button that console logs response
 document.querySelector("button").addEventListener("click", ()=>{
     console.debug("Doing some magic");
+    console.log(fetch("https://collectionapi.metmuseum.org/public/collection/v1/objects").then
+    ((response)=>{
+        console.log(response)
+    }));
 });
